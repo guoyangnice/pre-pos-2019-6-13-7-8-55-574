946fde0a91a2731dcbfa30133e1019a590650ef2
guoyangnice <yangjienice@126.com>
2019-08-21 12:59:33 +0800

    提交代码

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,36 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  
-  var result = [];
-  collectionB = collectionB.value;
-    for(var i = 0; i < collectionA.length; i ++) {
-        var temp = collectionA[i].key;
-        for(var j = 0; j < collectionB.length; j ++) {
-            if(temp === collectionB[j]) {
-                result.push(temp);
-                break;
-            }
-        }
+    var result = [];
+    var collectionC = collectionB.value;
+    var collectionD =[]
+    for(let i = 0; i < collectionA.length;i++){
+        collectionD.push(collectionA[i].key);
     }
-    return array_remove_repeat(result);
-}
 
-
-function array_remove_repeat(a) { // 去重
-  var r = [];
-  for(var i = 0; i < a.length; i ++) {
-      var flag = true;
-      var temp = a[i];
-      for(var j = 0; j < r.length; j ++) {
-          if(temp === r[j]) {
-              flag = false;å
-              break;
-          }
-      }
-      if(flag) {
-          r.push(temp);
-      }
-  }
-  return r;
+    //第一种方法
+    result = collectionD.filter(function(v){
+        return collectionC.indexOf(v)!==-1 // 利用filter方法来遍历是否有相同的元素
+    })
+    return result;
 }
